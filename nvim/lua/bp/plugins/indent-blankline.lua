137a1b80f0dd33e97e06e353eac9ae228a5a1add
Brady Phillips <brady.phillips99@gmail.com>
2021-09-08 21:26:58 +0100

    Add indent-blankline filetypes

--- nvim/lua/bp/plugins/indent-blankline.lua
+++ nvim/lua/bp/plugins/indent-blankline.lua
@@ -3,6 +3,14 @@ require'indent_blankline'.setup {
 	use_treesitter = true,
 	-- show_first_indent_level = false,
 	show_trailing_blankline_indent = false,
-	filetype_exclude = {'help', 'text', 'startify', 'packer'},
+	filetype_exclude = {
+		'help',
+		'text',
+		'startify',
+		'packer',
+		'NeogitStatus',
+		'NeogitLogView',
+		'NeogitPopup',
+	},
 	buftype_exclude = {'terminal'},
 }
