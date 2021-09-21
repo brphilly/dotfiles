98f10ec630f9e3b1dfb912b0756546fc320ba651
Brady Phillips <brady.phillips99@gmail.com>
2021-09-21 19:50:57 +0100

    Improve lualine config

--- nvim/lua/bp/plugins/lualine.lua
+++ nvim/lua/bp/plugins/lualine.lua
@@ -1,36 +1,49 @@
 -- TODO(brphilly): Investigate leaking file descriptors
-local function column()
-	return '%v'
-end
-local function line()
-	return '%l/%L'
-end
+local colours = require'nord.colours'
 
 require'lualine'.setup {
 	options = {
 		icons_enabled = true,
 		theme = 'mynord',
-		component_separators = {'', ''},
+		component_separators = {left = '', right = ''},
 		section_separators = '',
 		disabled_filetypes = {},
 	},
 	sections = {
 		lualine_a = {'mode'},
-		-- lualine_b = {'branch', 'diff'},
-		lualine_b = {'branch'},
+		lualine_b = {
+			'branch',
+			{
+				'diff',
+				diff_color = {
+					added = {fg = colours.green},
+					modified = {fg = colours.blues[1]},
+					removed = {fg = colours.red},
+				},
+			},
+		},
 		lualine_c = {{'filename', path = 0}},
-		lualine_x = {},
-		lualine_y = {'filetype'},
-		lualine_z = {column, line},
+		lualine_x = {'filetype'},
+		lualine_y = {{'diagnostics', sources = {'nvim_lsp'}}},
+		lualine_z = {'%v', '%l/%L'},
 	},
 	inactive_sections = {
 		lualine_a = {},
-		-- lualine_b = {'branch', 'diff'},
-		lualine_b = {'branch'},
+		lualine_b = {
+			'branch',
+			{
+				'diff',
+				diff_color = {
+					added = {fg = colours.green},
+					modified = {fg = colours.blues[1]},
+					removed = {fg = colours.red},
+				},
+			},
+		},
 		lualine_c = {{'filename', path = 0}},
 		lualine_x = {'filetype'},
-		lualine_y = {column, line},
-		lualine_z = {},
+		lualine_y = {{'diagnostics', sources = {'nvim_lsp'}}},
+		lualine_z = {'%v', '%l/%L'},
 	},
 	tabline = {},
 	extensions = {},
