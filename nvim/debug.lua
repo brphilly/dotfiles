a0c980ac852baebbcdf6ad8f1673d414cb73d401
Brady Phillips <brady.phillips99@gmail.com>
2021-09-23 06:28:51 +0100

    Change minimal config for debugging nvim/plugins

--- nvim/debug.lua
+++ nvim/debug.lua
@@ -1,37 +1,36 @@
--- This file is a minimal `init.lua` example for debugging plugins.
--- Open with `nvim -u debug.lua`
-
-local install_path = '/tmp/nvim/site/pack/packer/start/packer.nvim'
-local compile_path = install_path .. '/plugin/packer_compiled.lua'
-
+-- Taken from lewis6991/gitsigns.nvim
+vim.cmd [[set runtimepath=$VIMRUNTIME]]
 vim.cmd [[set packpath=/tmp/nvim/site]]
 
-local function load_plugins()
-	local packer = require'packer'
-	local use = packer.use
-	packer.init { compile_path = compile_path }
-	packer.startup {function()
-		use 'wbthomason/packer.nvim'
-		-- Put plugins to install here:
-		use {
-			'',
-		}
-	end,
-		config = { package_root = '/tmp/nvim/site/pack' },
-	}
-end
-
-_G.load_config = function()
-	-- Put any configuration here:
-end
-
+-- install packer
+local package_root = '/tmp/nvim/site/pack'
+local install_path = package_root .. '/packer/start/packer.nvim'
 if vim.fn.isdirectory(install_path) == 0 then
-	-- install packer
-	vim.fn.system { 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path }
-	load_plugins()
-	require'packer'.sync()
-	vim.cmd 'autocmd User PackerComplete ++once lua load_config()'
-else
-	load_plugins()
-	_G.load_config()
+	print("Installing plugins and dependencies.")
+	vim.fn.system { 'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path }
 end
+
+-- create function to run after installing plugins
+_G.load_config = function()
+	vim.cmd [[
+	runtime! plugin/**/*.vim
+	runtime! plugin/**/*.lua
+	]]
+end
+
+-- set load_config to run after installing plugins
+vim.cmd [[autocmd User PackerComplete ++once echo "Ready!" | lua load_config()]]
+
+-- Install plugins
+require('packer').startup {
+	{
+		'wbthomason/packer.nvim',
+		-- 'example/example.nvim',
+	},
+	config = {
+		package_root = package_root,
+		compile_path = install_path .. '/plugin/packer_compiled.lua',
+		display = { non_interactive = true },
+	},
+}
+require('packer').sync()
