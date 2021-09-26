fa7f3dbb76f691ea054e84295d30b3f7fd34b1d6
Brady Phillips <brady.phillips99@gmail.com>
2021-09-26 06:23:13 +0100

    Add insert on commit to neogit config

--- nvim/lua/bp/plugins/neogit/config.lua
+++ nvim/lua/bp/plugins/neogit/config.lua
@@ -1,4 +1,5 @@
 require'neogit'.setup {
+	disable_insert_on_commit = false,
 	integrations = {
 		-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
 		-- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
