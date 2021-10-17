fcc8326060fb9a288e631b35f88034332bab5c99
Brady Phillips <brady.phillips99@gmail.com>
2021-10-17 20:38:43 +0100

    Change nvim-treesitter-textobjects keybinds

--- nvim/lua/bp/plugins/nvim-treesitter-textobjects.lua
+++ nvim/lua/bp/plugins/nvim-treesitter-textobjects.lua
@@ -1,4 +1,3 @@
--- TODO(brphilly): Keep an eye on updates to this, goto next node, next string etc
 require("nvim-treesitter.configs").setup({
 	textobjects = {
 		select = {
@@ -8,49 +7,51 @@ require("nvim-treesitter.configs").setup({
 				-- You can use the capture groups defined in textobjects.scm
 				["if"] = "@function.inner",
 				["af"] = "@function.outer",
-				["ic"] = "@class.inner",
-				["ac"] = "@class.outer",
+				["i["] = "@block.inner",
+				["a["] = "@block.outer",
+				["ia"] = "@parameter.inner",
+				["aa"] = "@parameter.outer",
 			},
 		},
 		move = {
 			enable = true,
 			set_jumps = false, -- whether to set jumps in the jumplist
 			goto_next_start = {
-				["]m"] = "@function.outer",
-				["]]"] = "@class.outer",
+				["]f"] = "@function.outer",
+				["]]"] = "@block.outer",
 				["]a"] = "@parameter.inner",
 			},
 			goto_next_end = {
-				["]M"] = "@function.outer",
-				["]["] = "@class.outer",
-				["]A"] = "@parameter.inner",
+				["]gf"] = "@function.outer",
+				["]g]"] = "@block.outer",
+				["]ga"] = "@parameter.inner",
 			},
 			goto_previous_start = {
-				["[m"] = "@function.outer",
-				["[["] = "@class.outer",
+				["[f"] = "@function.outer",
+				["[["] = "@block.outer",
 				["[a"] = "@parameter.inner",
 			},
 			goto_previous_end = {
-				["[M"] = "@function.outer",
-				["[]"] = "@class.outer",
-				["[A"] = "@parameter.inner",
+				["[gf"] = "@function.outer",
+				["[g["] = "@block.outer",
+				["[ga"] = "@parameter.inner",
 			},
 		},
 		swap = {
 			enable = true,
 			swap_next = {
-				["<leader>]a"] = "@parameter.inner",
+				["]A"] = "@parameter.inner",
 			},
 			swap_previous = {
-				["<leader>[a"] = "@parameter.inner",
+				["[A"] = "@parameter.inner",
 			},
 		},
-		-- lsp_interop = {
-		--   enable = true,
-		--   peek_definition_code = {
-		--     ["df"] = "@function.outer",
-		--     ["dF"] = "@class.outer",
-		--   },
-		-- },
+		lsp_interop = {
+			enable = true,
+			border = "single",
+			peek_definition_code = {
+				["<leader>lp"] = "@block.outer",
+			},
+		},
 	},
 })
