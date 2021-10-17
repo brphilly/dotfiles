5207ed8ec2838a6c0b30ba27599fb316b5bc3c97
Brady Phillips <brady.phillips99@gmail.com>
2021-10-17 20:38:11 +0100

    Change treesitter incremental selection keybinds

--- nvim/lua/bp/plugins/nvim-treesitter.lua
+++ nvim/lua/bp/plugins/nvim-treesitter.lua
@@ -1,12 +1,7 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	ignore_install = {}, -- List of parsers to ignore installing
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = {}, -- list of language that will be disabled
-		custom_captures = {
-			-- ["foo.bar"] = "Identifier", -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-		},
+		enable = true,
 	},
 	indent = {
 		enable = true,
@@ -15,10 +10,9 @@ require("nvim-treesitter.configs").setup({
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = "<leader>v",
-			node_incremental = "<tab>",
-			node_decremental = "<s-tab>",
-			scope_incremental = "<leader><tab>",
+			node_incremental = "n",
+			node_decremental = "N",
+			scope_incremental = "<leader>n",
 		},
 	},
 })
