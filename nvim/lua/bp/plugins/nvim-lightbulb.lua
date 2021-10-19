308bead06ce8c2c57b7e44a18f708ab358a15a28
Brady Phillips <brady.phillips99@gmail.com>
2021-10-19 22:09:17 +0100

    Make nvim-lightbulb use signs instead of float

--- nvim/lua/bp/plugins/nvim-lightbulb.lua
+++ nvim/lua/bp/plugins/nvim-lightbulb.lua
@@ -2,10 +2,10 @@ vim.cmd(string.format(
 	[[
 		augroup nvim-lightbulb-update
 		autocmd!
-		autocmd CursorHold * lua require('nvim-lightbulb').update_lightbulb(%s)
+		autocmd CursorMoved * lua require('nvim-lightbulb').update_lightbulb(%s)
 		augroup END
 	]],
-	[[{ sign = { enabled = false }, float = { enabled = true, win_opts = { border = 'none' } }, status_text = { enabled = true } }]]
+	[[{ sign = { enabled = true, priority = 14 }, status_text = { enabled = true } }]]
 ))
 
 local c = require("nord.colours")
@@ -14,8 +14,11 @@ vim.cmd(string.format([[
 	augroup nvim-lightbulb-hl
 	autocmd!
 	autocmd ColorScheme nord highlight LightBulbFloatWin guibg=%s
+	autocmd ColorScheme nord highlight LightBulbSign guifg=%s
 	augroup END
 ]],
-	c.bg
+	c.bg,
+	c.danger
 ))
 vim.cmd(string.format([[doautocmd nvim-lightbulb-hl ColorScheme %s]], vim.g.colors_name))
+vim.fn.sign_define("LightBulbSign", { numhl = "LightBulbSign" })
