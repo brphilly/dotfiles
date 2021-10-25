56132648bb945a531ae7f3a3cfbc5b4776686695
Brady Phillips <brady.phillips99@gmail.com>
2021-10-25 02:49:22 +0100

    Don't show nvim-lightbulb sign

--- nvim/lua/bp/plugins/nvim-lightbulb.lua
+++ nvim/lua/bp/plugins/nvim-lightbulb.lua
@@ -2,10 +2,10 @@ vim.cmd(string.format(
 	[[
 		augroup nvim-lightbulb-update
 		autocmd!
-		autocmd CursorMoved * lua require('nvim-lightbulb').update_lightbulb(%s)
+		autocmd CursorHold * lua require('nvim-lightbulb').update_lightbulb(%s)
 		augroup END
 	]],
-	[[{ sign = { enabled = true, priority = 14 }, status_text = { enabled = true } }]]
+	[[{ sign = { enabled = false }, status_text = { enabled = true } }]]
 ))
 
 local c = require("nord.colours")
@@ -14,11 +14,9 @@ vim.cmd(string.format([[
 	augroup nvim-lightbulb-hl
 	autocmd!
 	autocmd ColorScheme nord highlight LightBulbFloatWin guibg=%s
-	autocmd ColorScheme nord highlight LightBulbSign guifg=%s
 	augroup END
 ]],
 	c.bg,
 	c.danger
 ))
 vim.cmd(string.format([[doautocmd nvim-lightbulb-hl ColorScheme %s]], vim.g.colors_name))
-vim.fn.sign_define("LightBulbSign", { numhl = "LightBulbSign" })
