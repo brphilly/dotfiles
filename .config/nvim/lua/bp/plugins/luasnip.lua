84b11adb93d8f675001e594b3c1444d6d95001b8
Brady Phillips <brady.phillips99@gmail.com>
2022-03-10 04:23:00 +0000

    Adjust some insert and select mode binds

--- .config/nvim/lua/bp/plugins/luasnip.lua
+++ .config/nvim/lua/bp/plugins/luasnip.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_set_keymap("i", "<c-]>]", "<cmd>lua require('luasnip').expand_or_jump()<cr>", { noremap = true })
-vim.api.nvim_set_keymap("s", "<c-]>]", "<cmd>lua require('luasnip').expand_or_jump()<cr>", { noremap = true })
-vim.api.nvim_set_keymap("i", "<c-]>[", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true })
-vim.api.nvim_set_keymap("s", "<c-]>[", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<c-l>", "<cmd>lua require('luasnip').expand_or_jump()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("s", "<c-l>", "<cmd>lua require('luasnip').expand_or_jump()<cr>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<c-h>", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true })
+vim.api.nvim_set_keymap("s", "<c-h>", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true })
 
 -- Write any lua snippets before this
 require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
