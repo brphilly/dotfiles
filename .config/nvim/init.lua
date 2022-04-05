1335a9dd50295ddb91ba5637e1b821fb7580b270
Brady Phillips <brady.phillips99@gmail.com>
2022-04-05 21:36:56 +0100

    Work around nvim bug #17867

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,12 @@ end
 -- New lua-based filetype detection
 vim.g.do_filetype_lua = 1
 
+-- work around #17867
+if vim.env.TERM == 'xterm-kitty' then
+  vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
+  vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
+end
+
 -- Disable builtin plugins:
 vim.g.loaded_gzip = 1
 vim.g.loaded_matchit = 1
