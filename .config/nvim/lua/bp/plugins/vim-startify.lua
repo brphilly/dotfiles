1415d61f9765dfc6f2f24fee4a96600241bde557
Brady Phillips <brady.phillips99@gmail.com>
2022-04-19 02:34:36 +0100

    Redo startify config

--- .config/nvim/lua/bp/plugins/vim-startify.lua
+++ .config/nvim/lua/bp/plugins/vim-startify.lua
@@ -1,15 +1,9 @@
 vim.g.startify_lists = {
 	{ type = "sessions", header = { "Recently Used Sessions" } },
-	{ type = "files", header = { "Recently Used Files" } },
-	{ type = "bookmarks", header = { "Bookmarks" } },
-}
-vim.g.startify_bookmarks = {
-	{ c = "~/.config/nvim/init.lua" },
-	{ s = "~/.config/zsh/zshrc" },
-	{ t = "term://zsh" },
+	{ type = "files", header = { "Recently Used Files" }, indices = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'} },
 }
 vim.g.startify_change_to_dir = 0
 vim.g.startify_fortune_use_unicode = 1
-vim.g.startify_session_number = 10
+vim.g.startify_session_number = 9
+vim.g.startify_files_number = 9
 vim.g.startify_session_sort = 1
-vim.g.startify_relative_path = 1
