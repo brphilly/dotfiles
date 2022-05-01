c309c0da73c3aba88dfe7788fd0cf48c0b6f1d2e
Brady Phillips <brady.phillips99@gmail.com>
2022-05-01 22:33:31 +0100

    Change luasnip mappings

--- .config/nvim/lua/bp/plugins/luasnip.lua
+++ .config/nvim/lua/bp/plugins/luasnip.lua
@@ -1,7 +1,7 @@
-vim.keymap.set("i", "<c-l>", require('luasnip').expand_or_jump)
-vim.keymap.set("s", "<c-l>", require('luasnip').expand_or_jump)
-vim.keymap.set("i", "<c-h>", function() require('luasnip').jump(-1) end)
-vim.keymap.set("s", "<c-h>", function() require('luasnip').jump(-1) end)
+vim.keymap.set("i", "<c-j>", require('luasnip').expand_or_jump)
+vim.keymap.set("s", "<c-j>", require('luasnip').expand_or_jump)
+vim.keymap.set("i", "<c-k>", function() require('luasnip').jump(-1) end)
+vim.keymap.set("s", "<c-k>", function() require('luasnip').jump(-1) end)
 
 -- Write any lua snippets before this
 require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
