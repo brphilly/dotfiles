ae7fb1ea76f388d457d23a54964b57f625dec6a5
Brady Phillips <brady.phillips99@gmail.com>
2022-05-01 22:32:28 +0100

    Remove cat alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,4 @@
 alias ls='exa'
 alias ll='exa -lag'
-alias cat='bat'
 alias pacdiff='sudo DIFFPROG="env XDG_CONFIG_HOME=/home/brady/.config XDG_DATA_HOME=/home/brady/.local/share nvim -d" pacdiff'
 alias lg='lazygit'
