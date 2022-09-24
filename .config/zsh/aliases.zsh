163a545300ca793f29800aa6bdb3b81670feb8a2
Brady Phillips <mail@brphilly.dev>
2022-09-24 04:09:18 +0100

    Add sudo alias to expand subsequent aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,4 @@
+alias sudo='sudo '
 alias ls='lsd'
 alias ll='lsd -la'
 alias pacdiff='sudo DIFFPROG="env XDG_CONFIG_HOME=/home/brady/.config XDG_DATA_HOME=/home/brady/.local/share nvim -d" pacdiff'
