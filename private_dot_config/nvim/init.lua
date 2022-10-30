7bda6d03dd6447e911bf5cea43732005937b5c37
Brady Phillips <mail@brphilly.dev>
2022-10-30 18:09:26 +0000

    Add chezmoi filetype detection

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,15 @@
 vim.g.mapleader = " "
 
 _G.P = function(...) vim.pretty_print(...) end
+
+-- chezmoi filetype detection
+vim.filetype.add({
+	filename = {
+		[vim.env.XDG_DATA_HOME.."/chezmoi/dot_zshenv"] = "zsh",
+		[vim.env.XDG_DATA_HOME.."/chezmoi/private_dot_config/zsh/dot_zshrc"] = "zsh",
+		[vim.env.XDG_DATA_HOME.."/chezmoi/private_dot_config/git/config"] = "gitconfig",
+		[vim.env.XDG_DATA_HOME.."/chezmoi/private_dot_config/git/ignore"] = "gitignore",
+		[vim.env.XDG_DATA_HOME.."/chezmoi/private_dot_ssh/config"] = "sshconfig",
+		["dot_editorconfig"] = "dosini",
+	},
+})
