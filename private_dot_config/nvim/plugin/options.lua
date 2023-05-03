be49d45cc6bb816a57ec3578f0a04eb18fa582d5
Brady Phillips <mail@brphilly.dev>
2023-05-03 20:47:28 +0100

    Move options around to match :options

--- private_dot_config/nvim/plugin/options.lua
+++ private_dot_config/nvim/plugin/options.lua
@@ -1,63 +1,81 @@
+-- 2 Moving, Searching and Patters
+vim.opt.cdhome = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- 4 Displaying Text
+vim.opt.virtualedit = { "onemore", "block" }
+vim.opt.smoothscroll = true
+vim.opt.scrolloff = 5
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "min:10,shift:1"
+vim.opt.sidescrolloff = 3
+vim.opt.fillchars = { eob = " ", fold = " " }
+vim.opt.list = true
+vim.opt.listchars = { extends = "»", precedes = "«", eol = "¬", trail = "∙", tab = "  ", nbsp = "␣" }
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- 5 Syntax, Highlighting and Spelling
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "151"
+vim.opt.cursorlineopt = "number"
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+vim.opt.spelloptions = "camel"
+
+-- 6 Multiple Windows
+vim.opt.laststatus = 3
+vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum}%s%C"
+vim.opt.switchbuf = "usetab"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- 10 Messages and Info
+vim.opt.shortmess:append("cI")
+vim.opt.showmode = false
+vim.opt.ruler = false
+
+-- 12 Editing Text
+vim.opt.undofile = true
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.pumheight = math.ceil(vim.go.lines / 2)
+vim.opt.showmatch = true
+
+-- 13 Tabs and Indenting
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 0
 vim.opt.shiftround = true
 vim.opt.cindent = true
 
-vim.opt.foldmethod = "indent"
-vim.opt.foldopen:remove({ "block", "hor", "percent" })
+-- 14 Folding
 vim.opt.foldlevel = 99
+vim.opt.foldopen:remove({ "block", "hor", "percent" })
+vim.opt.foldmethod = "indent"
 
-vim.opt.spelloptions = "camel"
-vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+-- 15 Diff Mode
+vim.opt.diffopt:append("linematch:60")
 
-vim.opt.sessionoptions:append({ "skiprtp", "terminal" })
+-- 16 Mapping
+vim.opt.timeoutlen = 3000
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "auto:1-2"
-vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum}%s%C"
-vim.opt.cursorlineopt = "number"
-vim.opt.cursorline = true
-vim.opt.colorcolumn = "151"
-vim.opt.ruler = false
-vim.opt.laststatus = 3
+-- 17 Reading and Writing Files
+vim.opt.backup = true
+vim.opt.backupdir = vim.env.XDG_DATA_HOME .. "/nvim/backup//"
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+-- 18 Swap File
+vim.opt.updatetime = 250
 
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.breakindentopt = "min:10,shift:1"
-
-vim.opt.virtualedit = { "onemore", "block" }
-vim.opt.smoothscroll = true
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 3
-vim.opt.list = true
-vim.opt.listchars = { extends = "»", precedes = "«", eol = "¬", trail = "∙", tab = "  ", nbsp = "␣" }
-vim.opt.fillchars = { eob = " ", fold = " " }
-vim.opt.showmode = false
-vim.opt.showmatch = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.inccommand = "split"
-
+-- 21 Make and Errors
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 vim.opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 
-vim.opt.termguicolors = true
-vim.opt.shortmess:append("cI")
+-- 22 Language Specific
 vim.opt.iskeyword:append("-")
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 3000
-vim.opt.switchbuf = "usetab"
 
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.pumheight = math.ceil(vim.go.lines / 2)
-
-vim.opt.diffopt:append("linematch:60")
-
-vim.opt.undofile = true
-vim.opt.backup = true
-vim.opt.backupdir = vim.env.XDG_DATA_HOME .. "/nvim/backup//"
+-- 24 Various
+vim.opt.virtualedit = { "onemore", "block" }
+vim.opt.sessionoptions:append({ "skiprtp", "terminal" })
+vim.opt.signcolumn = "auto:1-2"
+vim.opt.inccommand = "split"
