b142c2ede0b38b8858e8f725956e61c73d6ee893
Brady Phillips <mail@brphilly.dev>
2023-09-03 23:38:19 +0100

    Update cmp config

--- private_dot_config/nvim/lua/bp/plugins/nvim-cmp/init.lua
+++ private_dot_config/nvim/lua/bp/plugins/nvim-cmp/init.lua
@@ -1,4 +1,5 @@
 local cmp = require("cmp")
+local compare = require("cmp.config.compare")
 cmp.setup({
 	mapping = {
 		["<C-Space>"] = cmp.mapping.complete(),
@@ -64,7 +65,31 @@ cmp.setup({
 	},
 
 	sorting = {
-		priority_weight = 1,
+		priority_weight = 2,
+		comparators = {
+			compare.offset,
+			compare.exact,
+			-- compare.scopes,
+			compare.score,
+			-- copied from cmp-under
+			function(entry1, entry2)
+				local _, entry1_under = entry1.completion_item.label:find "^_+"
+				local _, entry2_under = entry2.completion_item.label:find "^_+"
+				entry1_under = entry1_under or 0
+				entry2_under = entry2_under or 0
+				if entry1_under > entry2_under then
+					return false
+				elseif entry1_under < entry2_under then
+					return true
+				end
+			end,
+			-- compare.recently_used,
+			-- compare.locality,
+			compare.kind,
+			-- compare.sort_text,
+			compare.length,
+			compare.order,
+		},
 	},
 
 	-- You should specify your *installed* sources.
