6ca1b5f34186fe475e3172dc214f92a85c9ac529
Brady Phillips <mail@brphilly.dev>
2023-09-12 12:31:31 +0100

    Update zsh cd widgets

--- private_dot_config/zsh/widgets.zsh
+++ private_dot_config/zsh/widgets.zsh
@@ -1,5 +1,5 @@
-cd-root() {
-	local dir=$(eval "fd --type d --exclude .git --exclude /sys --exclude /proc --exclude /dev --exclude /.btrfs-pool --exclude /run . / | fzf --height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS +m")
+cd-cmd() {
+	local dir=$(eval $1)
 	if [[ -z "$dir" ]]; then
 		zle redisplay
 		return 0
@@ -12,4 +12,24 @@ cd-root() {
 	zle reset-prompt
 	return $ret
 }
-zle -N cd-root
+
+cd-down() {
+	local dir="fd --type d --exclude .git --exclude /sys --exclude /proc --exclude /dev --exclude /.btrfs-pool --exclude /run . . | fzf --height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS +m"
+	cd-cmd $dir
+}
+zle -N cd-down
+
+parents() {
+	local input=$1
+    if [[ $input != "/" ]] then
+        local parent=$(dirname $input)
+        printf '%s\0' $parent
+        parents $parent
+    fi
+}
+
+cd-up() {
+	local dir="parents $(pwd) | fzf --read0 --height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS +m"
+	cd-cmd $dir
+}
+zle -N cd-up
