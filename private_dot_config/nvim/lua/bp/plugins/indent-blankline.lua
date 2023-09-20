e892bdec10a81c5b41488130065bcd3816defdf0
Brady Phillips <mail@brphilly.dev>
2023-09-20 22:23:08 +0100

    Make indent-blankline not show the current context start

--- private_dot_config/nvim/lua/bp/plugins/indent-blankline.lua
+++ private_dot_config/nvim/lua/bp/plugins/indent-blankline.lua
@@ -4,8 +4,6 @@ require("indent_blankline").setup({
 	use_treesitter = true,
 	show_trailing_blankline_indent = false,
 	show_current_context = true,
-	show_current_context_start = true,
-	context_start_priority = 125,
 	filetype_exclude = {
 		"help",
 		"text",
