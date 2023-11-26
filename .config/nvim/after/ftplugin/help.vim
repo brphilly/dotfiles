5feec1ae5d5217b0b50c2dc2efed9df07c0ad407
Brady Phillips <mail@brphilly.dev>
2023-11-26 22:54:18 +0000

    Remove unnecessary ftplugins

--- .config/nvim/after/ftplugin/help.vim
+++
@@ -1,6 +0,0 @@
-setlocal norelativenumber
-setlocal nonumber
-setlocal signcolumn=yes:1
-setlocal nolist
-setlocal nospell
-setlocal nobuflisted
--- .config/nvim/after/ftplugin/qf.lua
+++
@@ -1,4 +0,0 @@
-if vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].loclist ~= 1 then
-	vim.cmd("wincmd J")
-end
-vim.opt_local.colorcolumn = ""
