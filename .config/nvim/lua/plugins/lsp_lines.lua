c55e32167f2db24113c5e1d5a4a664ec539a5d6e
Brady Phillips <mail@brphilly.dev>
2023-12-03 18:58:24 +0000

    Show lsp_lines by default

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -4,7 +4,6 @@ return {
 		event = {"CursorMoved", "CursorHold"},
 		config = function()
 			require("lsp_lines").setup()
-			vim.diagnostic.config({virtual_lines = false})
 			vim.keymap.set("n", "<leader><leader>", require("lsp_lines").toggle)
 		end,
 	},
