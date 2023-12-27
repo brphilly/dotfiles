fc7d2c3c18a9f25267be0d77a3f0f1e9e4b9f546
Brady Phillips <mail@brphilly.dev>
2023-12-27 16:00:37 +0000

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+source $XDG_CONFIG_HOME/fish/env.fish
+
+if status is-interactive
+	set -g fish_greeting
+
+	abbr -a ls --position anywhere lsd
+	abbr -a ll --position anywhere 'lsd -la'
+	abbr -a cat --position anywhere bat
+	abbr -a lg lazygit
+	abbr -a yz yazi
+	abbr -a pacdiff 'sudo DIFFPROG="env XDG_CONFIG_HOME=/home/brady/.config XDG_DATA_HOME=/home/brady/.local/share nvim -d" pacdiff'
+	abbr -a paca 'paru --color always -Sl | sed -E \'s: :/:; s/ unknown-version//\' | fzf --multi --ansi --nth 1 --tiebreak index --preview \'paru -Si {1}\' | cut -d \' \' -f 1 | xargs -rot paru -S'
+	abbr -a pacr 'paru --color always -Q | fzf --multi --ansi --nth 1 --preview \'paru -Qi {1}\' | cut -d \' \' -f 1 | xargs -rot paru -Rs'
+
+	starship init fish | source
+	zoxide init fish | source
+end
