42f18198afb876279126a94cab02a26fde68a341
Brady Phillips <mail@brphilly.dev>
2023-12-27 03:57:19 +0000

    Fix wordmotion lazy loading

--- .config/nvim/lua/plugins/wordmotion.lua
+++ .config/nvim/lua/plugins/wordmotion.lua
@@ -2,6 +2,7 @@ return {
 	{
 		url = "https://github.com/chaoren/vim-wordmotion",
 		keys = "<space>",
+		event = "ModeChanged *:no*",
 		init = function() vim.g.wordmotion_prefix = "<space>" end,
 	},
 }
