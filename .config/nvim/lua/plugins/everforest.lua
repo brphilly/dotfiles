e2f094b2443e0caef13ee9c148bee21eac4342ed
Brady Phillips <mail@brphilly.dev>
2024-01-15 00:01:23 +0000

    Switch to everforest-nvim

--- .config/nvim/lua/plugins/everforest.lua
+++ .config/nvim/lua/plugins/everforest.lua
@@ -1,21 +1,10 @@
 return {
 	{
-		url = "https://github.com/sainnhe/everforest",
+		url = "https://github.com/neanias/everforest-nvim",
 		lazy = false,
 		priority = 1000,
-		init = function()
-			vim.g.everforest_background = "hard"
-			vim.g.everforest_diagnostic_virtual_text = "colored"
-			vim.g.everforest_enable_italic = 1
-			vim.g.everforest_ui_contrast = "high"
-			vim.api.nvim_create_autocmd('ColorScheme', {pattern = 'everforest',
-				callback = function()
-					vim.api.nvim_set_hl(0, 'TelescopeNormal', {link = "NormalFloat"})
-					vim.api.nvim_set_hl(0, 'TelescopeBorder', {link = "FloatBorder"})
-				end,
-			})
-		end,
 		config = function()
+			require("everforest").setup({background = "hard", italics = true, ui_contrast = "high"})
 			vim.cmd("colorscheme everforest")
 		end,
 	},
