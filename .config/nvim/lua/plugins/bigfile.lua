8e861d072515e0501cdaa1672ce37af97d659808
Brady Phillips <mail@brphilly.dev>
2024-01-17 19:26:00 +0000

    Update bigfile config

--- .config/nvim/lua/plugins/bigfile.lua
+++ .config/nvim/lua/plugins/bigfile.lua
@@ -2,5 +2,16 @@ return {
 	{
 		url = "https://github.com/LunarVim/bigfile.nvim",
 		lazy = false,
+		opts = {
+			features = {
+				{
+					name = "foldcolumn",
+					opts = { defer = false },
+					disable = function()
+						vim.wo.foldcolumn = "0"
+					end,
+				},
+			},
+		},
 	},
 }
