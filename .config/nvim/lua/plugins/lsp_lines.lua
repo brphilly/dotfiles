5241cdb01d9f22d8dc3434ca2b125e72a24dd70d
Brady Phillips <mail@brphilly.dev>
2024-01-28 19:50:25 +0000

    Use only_current_line in lsp_lines

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -4,8 +4,24 @@ return {
 		event = "DiagnosticChanged",
 		config = function()
 			require("lsp_lines").setup()
-			vim.diagnostic.config({ virtual_lines = false })
-			vim.keymap.set("n", "gl", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+			vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+			vim.keymap.set("n", "gl", function()
+				local diag_conf = vim.diagnostic.config()
+				if diag_conf.virtual_lines then
+					vim.diagnostic.config({ virtual_lines = false })
+				else
+					vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+				end
+			end, { desc = "Toggle current line lsp_lines" })
+
+			vim.keymap.set("n", "gL", function()
+				local diag_conf = vim.diagnostic.config()
+				if diag_conf.virtual_lines == true then
+					vim.diagnostic.config({ virtual_lines = false })
+				else
+					vim.diagnostic.config({ virtual_lines = true })
+				end
+			end, { desc = "Toggle all lsp_lines" })
 		end,
 	},
 }
