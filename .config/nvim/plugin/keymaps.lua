ddee930f5ee2cc4d78e840c83b404d293a081c58
Brady Phillips <mail@brphilly.dev>
2024-02-13 22:34:16 +0000

    Update keymaps and remove keymap-funcs

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -14,11 +14,6 @@ vim.keymap.set("i", "<cr>", [[pumvisible() ? "<c-y>" : "<cr>"]], { expr = true }
 vim.keymap.set("n", "<c-o>", "<c-o>zz")
 vim.keymap.set("n", "<c-i>", "<c-i>zz")
 
-vim.keymap.set("n", "zh", "zH")
-vim.keymap.set("n", "zH", "zh")
-vim.keymap.set("n", "zL", "zl")
-vim.keymap.set("n", "zl", require("bp.keymap-funcs").hor_scroll)
-
 vim.keymap.set("x", "p", '"_dP')
 vim.keymap.set("x", "<leader>p", '"_d"+P')
 vim.keymap.set("n", "<leader>p", '"+p')
@@ -60,28 +55,46 @@ end)
 vim.keymap.set("n", "'g", "<cmd>terminal lazygit<cr>i")
 
 vim.keymap.set("n", "<leader>cv", "<cmd>qall<cr>")
-vim.keymap.set("n", "<leader>cb", require("bp.keymap-funcs").buf_close)
-vim.keymap.set("n", "<leader>ch", require("bp.keymap-funcs").buf_close_hid)
-vim.keymap.set("n", "<leader>cu", require("bp.keymap-funcs").buf_close_unlist)
+vim.keymap.set("n", "<leader>cb", "<cmd>bd<cr>")
 vim.keymap.set("n", "<leader>ct", "<cmd>tabclose<cr>")
 
 vim.keymap.set("n", "[b", "<cmd>bprevious<cr>")
 vim.keymap.set("n", "]b", "<cmd>bnext<cr>")
 vim.keymap.set("n", "[B", "<cmd>bfirst<cr>")
 vim.keymap.set("n", "]B", "<cmd>blast<cr>")
-vim.keymap.set("n", "''", require("bp.keymap-funcs").switch_prev_buf)
+vim.keymap.set("n", "''", function()
+	if vim.fn.buflisted(0) == 1 then
+		vim.cmd("buffer #")
+	else
+		vim.cmd("bprevious")
+	end
+end, { desc = "Switch to previous buffer" })
 
 vim.keymap.set("n", "[q", "<cmd>cprev<cr>")
 vim.keymap.set("n", "]q", "<cmd>cnext<cr>")
 vim.keymap.set("n", "[Q", "<cmd>cfirst<cr>")
 vim.keymap.set("n", "]Q", "<cmd>clast<cr>")
-vim.keymap.set("n", "<c-w><c-space>", require("bp.keymap-funcs").toggle_qf)
+vim.keymap.set("n", "<c-w><c-space>", function()
+	local winid = vim.fn.getqflist({ winid = 0 }).winid
+	if winid ~= 0 then
+		vim.cmd("cclose")
+	elseif not vim.tbl_isempty(vim.fn.getqflist()) then
+		vim.cmd("copen")
+	end
+end, { desc = "Toggle quickfix list window" })
 
 vim.keymap.set("n", "[l", "<cmd>lprev<cr>")
 vim.keymap.set("n", "]l", "<cmd>lnext<cr>")
 vim.keymap.set("n", "[L", "<cmd>lfirst<cr>")
 vim.keymap.set("n", "]L", "<cmd>llast<cr>")
-vim.keymap.set("n", "<c-w><space>", require("bp.keymap-funcs").toggle_ll)
+vim.keymap.set("n", "<c-w><space>", function()
+	local winid = vim.fn.getloclist(0, { winid = 0 }).winid
+	if winid ~= 0 then
+		vim.cmd("lclose")
+	elseif not vim.tbl_isempty(vim.fn.getloclist(0)) then
+		vim.cmd("lopen")
+	end
+end, { desc = "Toggle location list window" })
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
