a11a9b418c2d56b0c7c88edc3fb64afbbe169a26
Brady Phillips <mail@brphilly.dev>
2024-02-18 18:51:05 +0000

    Redo nvim-ufo config

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -8,12 +8,37 @@ return {
 		config = function()
 			vim.o.foldlevel = 99
 			vim.o.foldlevelstart = 99
-			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-			vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-			vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
 
-			require("ufo").setup()
+			vim.keymap.set("n", "zR", function()
+				vim.b.ufo_foldlevel = 99
+				require("ufo").openAllFolds()
+			end)
+			vim.keymap.set("n", "zM", function()
+				vim.b.ufo_foldlevel = 0
+				require("ufo").closeAllFolds()
+			end)
+			vim.keymap.set("n", "zr", function()
+				vim.b.ufo_foldlevel = (vim.b.ufo_foldlevel or 99) + vim.v.count1
+				require("ufo").closeFoldsWith(vim.b.ufo_foldlevel)
+			end)
+			vim.keymap.set("n", "zm", function()
+				vim.b.ufo_foldlevel = math.max((vim.b.ufo_foldlevel or 99) - vim.v.count1, 0)
+				require("ufo").closeFoldsWith(vim.b.ufo_foldlevel)
+			end)
+
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					local ts_lang = vim.treesitter.language.get_lang(filetype)
+					local has_ts_folds = ts_lang and vim.treesitter.query.get(ts_lang, "folds")
+					if buftype == "" and has_ts_folds then
+						return { "lsp", "treesitter" }
+					elseif has_ts_folds then
+						return { "treesitter", "indent" }
+					else
+						return "indent"
+					end
+				end,
+			})
 		end,
 	},
 }
