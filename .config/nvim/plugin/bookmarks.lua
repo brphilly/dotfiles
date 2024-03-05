c84641ab89fc4cd0f01d751971edb7edab153ed0
Brady Phillips <mail@brphilly.dev>
2024-03-05 00:35:02 +0000

    fix: use lua variable for bookmarks, not vimscript

--- .config/nvim/plugin/bookmarks.lua
+++ .config/nvim/plugin/bookmarks.lua
@@ -1,5 +1,5 @@
-vim.g.bookmarks = {}
-vim.g.git_head = ""
+Bookmarks = {}
+local git_head = ""
 vim.api.nvim_set_hl(0, "bookmarks", { fg = "#7fbbb3" })
 local ns = vim.api.nvim_create_namespace("bookmarks")
 local aug = vim.api.nvim_create_augroup("bookmarks", {})
@@ -7,20 +7,20 @@ local bookmarks_dir = vim.fn.stdpath("state") .. "/bookmarks/" .. vim.fs.normali
 vim.fn.mkdir(bookmarks_dir, "p", tonumber("700", 8))
 
 local function read_bookmarks_file()
-	local bookmarks_file = bookmarks_dir .. string.format("/%s.json", vim.g.git_head)
+	local bookmarks_file = bookmarks_dir .. string.format("/%s.json", git_head)
 
 	local file = io.open(bookmarks_file)
 	if file ~= nil then
 		local json = file:read("*a")
-		vim.g.bookmarks = vim.json.decode(json)
+		Bookmarks = vim.json.decode(json)
 	else
-		vim.g.bookmarks = {}
+		Bookmarks = {}
 	end
 end
 local function place_bookmark_extmarks(buf)
 	vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
 	local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":.")
-	local extmrks = vim.tbl_keys(vim.g.bookmarks[fname] or {})
+	local extmrks = vim.tbl_keys(Bookmarks[fname] or {})
 	for extmrk in vim.iter(extmrks) do
 		local row, col = table.unpack(vim.iter(vim.gsplit(extmrk, ",", { plain = true })):map(tonumber):totable())
 		vim.api.nvim_buf_set_extmark(buf, ns, row, col, {
@@ -39,8 +39,8 @@ local function watch_git_head()
 		{ text = true },
 		vim.schedule_wrap(function(obj)
 			local git_dir, head = table.unpack(vim.split(obj.stdout, "\n", { plain = true }))
-			if head ~= vim.g.git_head then
-				vim.g.git_head = head
+			if head ~= git_head then
+				git_head = head
 				read_bookmarks_file()
 				local bufs = vim.iter(vim.api.nvim_list_bufs()):filter(function(buf)
 					return vim.api.nvim_get_option_value("buftype", { buf = buf }) == ""
@@ -67,10 +67,10 @@ vim.api.nvim_create_autocmd("BufRead", {
 })
 
 local function write_bookmarks_file()
-	local bookmarks_file = bookmarks_dir .. string.format("/%s.json", vim.g.git_head)
+	local bookmarks_file = bookmarks_dir .. string.format("/%s.json", git_head)
 	local file = io.open(bookmarks_file, "w")
 	if file ~= nil then
-		file:write(vim.json.encode(vim.g.bookmarks))
+		file:write(vim.json.encode(Bookmarks))
 		file:close()
 	end
 end
@@ -86,9 +86,9 @@ vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
 
 		local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(args.buf), ":.")
 		if vim.tbl_isempty(bkmrk_locs) then
-			vim.g.bookmarks[fname] = nil
+			Bookmarks[fname] = nil
 		else
-			vim.g.bookmarks[fname] = bkmrk_locs
+			Bookmarks[fname] = bkmrk_locs
 		end
 		write_bookmarks_file()
 	end,
@@ -104,18 +104,18 @@ local function toggle_bookmark()
 			local extmrk_id, row, col = table.unpack(extmrk)
 			vim.api.nvim_buf_del_extmark(0, ns, extmrk_id)
 
-			if vim.g.bookmarks[fname] ~= nil then
-				vim.g.bookmarks[fname][table.concat({ row, col }, ",")] = nil
-				if vim.tbl_isempty(vim.g.bookmarks[fname]) then
-					vim.g.bookmarks[fname] = nil
+			if Bookmarks[fname] ~= nil then
+				Bookmarks[fname][table.concat({ row, col }, ",")] = nil
+				if vim.tbl_isempty(Bookmarks[fname]) then
+					Bookmarks[fname] = nil
 				end
 			end
 		end
 	else
-		if vim.g.bookmarks[fname] == nil then
-			vim.g.bookmarks[fname] = { [table.concat({ row, col }, ",")] = true }
+		if Bookmarks[fname] == nil then
+			Bookmarks[fname] = { [table.concat({ row, col }, ",")] = true }
 		else
-			vim.g.bookmarks[fname][table.concat({ row, col }, ",")] = true
+			Bookmarks[fname][table.concat({ row, col }, ",")] = true
 		end
 		vim.api.nvim_buf_set_extmark(0, ns, row, col, {
 			sign_text = "⚑",
