7cb3efc1c3e822404501393d85601d5957110d68
Brady Phillips <mail@brphilly.dev>
2024-03-17 17:17:14 +0000

    Add nvim-ts-autotag

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -4,4 +4,16 @@ return {
 		event = "InsertEnter",
 		opts = {},
 	},
+	{
+		url = "https://github.com/windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				autotag = {
+					enable = true,
+				},
+			})
+			require("nvim-ts-autotag").setup({})
+		end,
+	},
 }
