6f69becec63822cb89b169985c1822928bc23eda
Brady Phillips <mail@brphilly.dev>
2024-05-20 14:49:37 +0100

    chore: update plugins

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -7,13 +7,6 @@ return {
 	{
 		url = "https://github.com/windwp/nvim-ts-autotag",
 		event = "InsertEnter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				autotag = {
-					enable = true,
-				},
-			})
-			require("nvim-ts-autotag").setup({})
-		end,
+		config = true,
 	},
 }
