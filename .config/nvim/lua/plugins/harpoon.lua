c49435148a93bc4d87a603500fcd07a79e6cc1af
Brady Phillips <mail@brphilly.dev>
2024-07-08 23:59:48 +0100

    feat: add harpoon

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		url = "https://github.com/ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		lazy = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = true,
+		init = function()
+			vim.keymap.set("n", "<leader>m", function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+			vim.keymap.set("n", "m<leader>", function()
+				require("harpoon"):list():add()
+			end)
+			vim.keymap.set("n", "[b", function()
+				require("harpoon"):list():prev()
+			end)
+			vim.keymap.set("n", "]b", function()
+				require("harpoon"):list():next()
+			end)
+			for i = 1, 9 do
+				vim.keymap.set("n", "m" .. i, function()
+					require("harpoon"):list():select(i)
+				end)
+			end
+		end,
+	},
+}
