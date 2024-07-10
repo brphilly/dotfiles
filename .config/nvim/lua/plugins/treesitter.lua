80241419870a4d93eab76b372f96c16cfd585da4
Brady Phillips <mail@brphilly.dev>
2024-07-10 17:30:18 +0100

    fix: cursor not sticking to column with j/k

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -96,5 +96,16 @@ return {
 	{
 		url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring",
 		lazy = false,
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+			local get_option = vim.filetype.get_option
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+						and require("ts_context_commentstring.internal").calculate_commentstring()
+					or get_option(filetype, option)
+			end
+		end,
 	},
 }
