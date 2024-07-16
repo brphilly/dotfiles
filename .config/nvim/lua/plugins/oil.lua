3861bd11d5802e0870fa784bf5b156cd53ccb7fd
Brady Phillips <mail@brphilly.dev>
2024-07-17 00:53:37 +0100

    fix: don't lazy load oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,6 @@ return {
 	{
 		url = "https://github.com/stevearc/oil.nvim",
 		dependencies = { "echasnovski/mini.icons" },
-		cmd = "Oil",
 		opts = {
 			delete_to_trash = true,
 			win_options = {
@@ -17,7 +16,6 @@ return {
 	{
 		url = "https://github.com/refractalize/oil-git-status.nvim",
 		dependencies = { "stevearc/oil.nvim" },
-		cmd = "Oil",
 		opts = {},
 	},
 }
