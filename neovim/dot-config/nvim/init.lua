5b453feaddee31bb8a1df6d5b9bbb36622f27e26
Brady Phillips <mail@brphilly.dev>
2024-12-22 00:08:39 +0000

    fix: remove workaround for neovim bug
    Fixed in neovim 0.10.3

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -14,17 +14,6 @@ require("keymaps")
 require("autocmds")
 require("diagnostics")
 
--- Workaround for https://github.com/neovim/neovim/issues/30985
-for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-	local default_diagnostic_handler = vim.lsp.handlers[method]
-	vim.lsp.handlers[method] = function(err, result, context, config)
-		if err ~= nil and err.code == -32802 then
-			return
-		end
-		return default_diagnostic_handler(err, result, context, config)
-	end
-end
-
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
