b1b672e683054bfc2785b7cd43da2f3c372e0e08
Brady Phillips <mail@brphilly.dev>
2024-12-22 02:30:59 +0000

    feat: swap nvim-cmp for blink.cmp

---
+++ neovim/dot-config/nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		url = "https://github.com/saghen/blink.cmp",
+		version = "*",
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "super-tab" },
+		},
+	},
+}
