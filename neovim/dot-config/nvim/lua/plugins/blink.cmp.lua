e383b178301e78b266ce2f41b4ac3ebcfb52ec0c
Brady Phillips <mail@brphilly.dev>
2025-03-15 03:42:03 +0000

    feat: remove avante.nvim

--- neovim/dot-config/nvim/lua/plugins/blink.cmp.lua
+++ neovim/dot-config/nvim/lua/plugins/blink.cmp.lua
@@ -20,29 +20,6 @@ return {
 					"path",
 					"buffer",
 					"snippets",
-					"avante_commands",
-					"avante_files",
-					"avante_mentions",
-				},
-				providers = {
-					avante_commands = {
-						name = "avante_commands",
-						module = "blink.compat.source",
-						score_offset = 90, -- show at a higher priority than lsp
-						opts = {},
-					},
-					avante_files = {
-						name = "avante_files",
-						module = "blink.compat.source",
-						score_offset = 100, -- show at a higher priority than lsp
-						opts = {},
-					},
-					avante_mentions = {
-						name = "avante_mentions",
-						module = "blink.compat.source",
-						score_offset = 1000, -- show at a higher priority than lsp
-						opts = {},
-					},
 				},
 			},
 		},
