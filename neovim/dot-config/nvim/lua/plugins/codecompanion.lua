b31f7cd45b65fbeb48359494498bbb6dc52897e0
Brady Phillips <mail@brphilly.dev>
2025-03-15 03:42:19 +0000

    feat: add codecompanion.nvim

---
+++ neovim/dot-config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+			},
+			adapters = {
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						env = {
+							api_key = "cmd:secret-tool lookup api anthropic",
+						},
+					})
+				end,
+			},
+		},
+	},
+}
--- neovim/dot-config/nvim/lua/plugins/markview.lua
+++ neovim/dot-config/nvim/lua/plugins/markview.lua
@@ -2,5 +2,11 @@ return {
 	{
 		"OXY2DEV/markview.nvim",
 		lazy = false,
+		opts = {
+			preview = {
+				filetypes = { "markdown", "rmd", "quarto", "typst", "codecompanion" },
+				ignore_buftypes = {},
+			},
+		},
 	},
 }
