3fb6a2c3511f05a02775ab5ac9f5dcd5e5d9613a
Brady Phillips <mail@brphilly.dev>
2025-06-16 16:17:40 +0100

    Swap rye for uv

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -17,7 +17,7 @@ if status is-interactive
 	abbr -a paca 'paru --color always -Sl | sed -E \'s: :/:; s/ unknown-version//\' | fzf --multi --ansi --nth 1 --tiebreak index --preview \'paru -Si {1}\' | cut -d \' \' -f 1 | xargs -rot paru -S'
 	abbr -a pacr 'paru --color always -Q | fzf --multi --ansi --nth 1 --preview \'paru -Qi {1}\' | cut -d \' \' -f 1 | xargs -rot paru -Rs'
 
-	fish_add_path -gP ~/.rye/shims
+	fish_add_path -gP ~/.local/bin
 	fish_add_path -gP ~/.cargo/bin
 	starship init fish | source
 	zoxide init fish | source
