e406ed9e6310fa976fd51e5e52a8810a4864c4ce
Brady Phillips <mail@brphilly.dev>
2025-06-28 15:09:35 +0100

    Update plugins

--- neovim/dot-config/nvim/lua/plugins/markview.lua
+++ neovim/dot-config/nvim/lua/plugins/markview.lua
@@ -2,6 +2,8 @@ return {
 	{
 		"OXY2DEV/markview.nvim",
 		lazy = false,
+		-- Force markview.nvim to be placed before treesitter in the rtp
+		priority = 49,
 		opts = {
 			preview = {
 				filetypes = { "markdown", "rmd", "quarto", "typst", "codecompanion" },
