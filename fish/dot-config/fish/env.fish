b76892ccb82e67e718ce3ac588989ebefb779f74
Brady Phillips <mail@brphilly.dev>
2025-10-13 17:02:56 +0100

    Add fzf env vars

--- fish/dot-config/fish/env.fish
+++ fish/dot-config/fish/env.fish
@@ -1,2 +1,4 @@
 export LS_COLORS="$(vivid generate iceberg-dark)"
 export BAT_THEME="gruvbox-dark"
+export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+export FZF_DEFAULT_OPTS="--cycle --layout=reverse --bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
